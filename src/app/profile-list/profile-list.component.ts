8c8deb182ec482a30cdad38a802b98062c64b14e
Mateusz Molka <mateuszmolka@SHUTTONQU374.local>
2018-01-12 21:48:18 +0000

    date formatting

--- src/app/profile-list/profile-list.component.ts
+++ src/app/profile-list/profile-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
 import { Profile } from '../interfaces/profile'
 import { ProfileService } from '../services/profile.service';
 
